601cf11bf31a958223232c8b30346431ba7c3790
Aaron James <49303118+torpisoul@users.noreply.github.com>
2023-08-19 15:05:42 +0100

    Added javascript
    Added some logic and improvements for category selections. Still work in progress but is currently working well enough.

--- index.js
+++ index.js
@@ -1,92 +1,18 @@
-var subCategories = {
-    "Characters": [
-        "Chris Reynolds",
-        "Cathy Turner",
-        "Jasmine Bennett",
-        "Louise Montgomery",
-        "Clare Parker",
-        "Martyn Anderson",
-        "Aaron Roberts",
-        "Chloe Mitchell",
-        "Emily Thompson (Servant)",
-        "Victor Vanstone (Murder Victim)"
-    ],
-    "Character Descriptions": [
-        "Physical Traits", 
-        "Personality Traits", 
-        "Background (history)", 
-        "Occupation (skills)", 
-        "Quirks (habits)"
-    ],
-    "Character Relationships": [
-        "Partner", 
-        "Friend", 
-        "Sibling", 
-        "Colleague", 
-        "Stranger"
-    ],
-    "Clues": [
-        "Written Messages", 
-        "Physical Objects", 
-        "Hidden Documents", 
-        "Cryptic Symbols", 
-        "Puzzle Pieces"
-    ],
-    "Evidence": [
-        "Fingerprints", 
-        "Footprints", 
-        "DNA Samples", 
-        "Surveilance Footage", 
-        "Audio Recordings"
-    ],
-    "Motive": [
-        "Greed", 
-        "Revenge", 
-        "Jealousy", 
-        "Blackmail", 
-        "Desire for Power"
-    ],
-    "Players": [
-        "Player 1", 
-        "Player 2", 
-        "Player 3", 
-        "Player 4", 
-        "Player 5", 
-        "Player 6", 
-        "Player 7", 
-        "Player 8"
-    ],
-    "Red Herons": [
-        "Misleading Clues", 
-        "False Alibis", 
-        "Suspicious Behaviours", 
-        "Contradictory Statements", 
-        "Misinterpreted Evidence"
-    ],
-    "Theme": [
-        "Historical Era", 
-        "Fantasy World", 
-        "Sci-Fi", 
-        "Noir", 
-        "Modern-Day"
-    ],
-};
-
 var categorySelect = document.getElementById("category");
-var subCategorySelect = document.getElementById("subcategory");
+var subcategorySelect = document.getElementById("subcategory");
 
-categorySelect.addEventListener("change", function() {
+categorySelect.addEventListener("change", function () {
     var selectedCategory = categorySelect.value;
     subCategorySelect.innerHTML = ""; //clear existing options
 
     if (selectedCategory !== "") {
-        var subCategoryOptions = subCategories[selectedCategory];
-        if (subCategoryOptions) {
-            subCategoryOptions.forEach(function(option) {
+        var subcategoryOptions = subcategories[selectedCategory];
+        if (subcategoryOptions) {
+            subcategoryOptions.forEach(function (option) {
                 var optionElement = document.createElement("option");
                 optionElement.value = option;
                 optionElement.textContent = option;
-                subCategorySelect.appendChild(optionElement);
+                subcategorySelect.appendChild(optionElement);
             });
         }
     }
@@ -96,6 +22,131 @@ categorySelect.addEventListener("change", function() {
         var defaultOption = document.createElement("option");
         defaultOption.value = "";
         defaultOption.textContent = "Choose a Sub-Category";
-        subCategorySelect.appendChild(defaultOption);
+        subcategorySelect.appendChild(defaultOption);
     }
 });
+
+//button collapse onclick
+document.addEventListener("DOMContentLoaded", function () {
+    const categoryButtons = document.querySelectorAll(".category-button");
+    const resetButton = document.querySelector(".category-reset-button");
+    const subcategoryContainer = document.getElementById("subcategory");
+
+    //the subCategories variable should import data from the database, when functional.
+    var subcategories = {
+        "characters": [
+            "Chris Reynolds",
+            "Cathy Turner",
+            "Jasmine Bennett",
+            "Louise Montgomery",
+            "Clare Parker",
+            "Martyn Anderson",
+            "Aaron Roberts",
+            "Chloe Mitchell",
+            "Emily Thompson (Servant)",
+            "Victor Vanstone (Murder Victim)"
+        ],
+        "characterTraits": [
+            "Physical Traits",
+            "Personality Traits",
+            "Background (history)",
+            "Occupation (skills)",
+            "Quirks (habits)"
+        ],
+        "relationships": [
+            "Partner",
+            "Friend",
+            "Sibling",
+            "Colleague",
+            "Stranger"
+        ],
+        "clues": [
+            "Written Messages",
+            "Physical Objects",
+            "Hidden Documents",
+            "Cryptic Symbols",
+            "Puzzle Pieces"
+        ],
+        "evidence": [
+            "Fingerprints",
+            "Footprints",
+            "DNA Samples",
+            "Surveilance Footage",
+            "Audio Recordings"
+        ],
+        "motive": [
+            "Greed",
+            "Revenge",
+            "Jealousy",
+            "Blackmail",
+            "Desire for Power"
+        ],
+        "players": [
+            "Player 1",
+            "Player 2",
+            "Player 3",
+            "Player 4",
+            "Player 5",
+            "Player 6",
+            "Player 7",
+            "Player 8"
+        ],
+        "redHerons": [
+            "Misleading Clues",
+            "False Alibis",
+            "Suspicious Behaviours",
+            "Contradictory Statements",
+            "Misinterpreted Evidence"
+        ],
+        "theme": [
+            "Historical Era",
+            "Fantasy World",
+            "Sci-Fi",
+            "Noir",
+            "Modern-Day"
+        ],
+    };
+
+    categoryButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            const categoryValue = button.getAttribute("data-value");
+            const subcategoryOptions = subcategories[categoryValue];
+
+            if (subcategoryOptions) {
+                subcategoryContainer.innerHTML = ""; //clear existing subcategories
+
+                subcategoryOptions.forEach(subcategoryOption => {
+                    const subcategoryButton = document.createElement("button");
+                    subcategoryButton.textContent = subcategoryOption;
+                    subcategoryButton.classList.add("subcategory-button");
+                    subcategoryContainer.appendChild(subcategoryButton);
+                });
+
+                resetButton.removeAttribute("disabled");
+
+                subcategoryContainer.scrollIntoView({
+                    behavior: "smooth",
+                    block: "end"
+                });
+            }
+
+            categoryButtons.forEach(otherButton => {
+                if (otherButton !== button) {
+                    otherButton.classList.add("collapsed");
+                    resetButton.removeAttribute("disabled");
+                }
+            });
+            button.classList.remove("collapsed");
+        });
+    });
+
+    resetButton.addEventListener("click", () => {
+        categoryButtons.forEach(otherButton => {
+            otherButton.classList.remove("collapsed");
+        });
+        resetButton.setAttribute("disabled", "disabled");
+        
+        subcategoryContainer.innerHTML = ""; //clear existing subcategories
+    });
+});
+
